89935a30707a44a016217302f6efdf69f184ec41
vividxz <80575342+vividxz@users.noreply.github.com>
2021-04-05 21:48:25 +0530

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Addition
+62+432
+
+
+# In[6]:
+
+
+#Substraction
+4-7
+
+
+# In[7]:
+
+
+
+#Multiplication 
+72*4
+
+
+# In[11]:
+
+
+#Division
+5/2
+
+
+# In[12]:
+
+
+#Floor division
+5//2
+
+
+# In[13]:
+
+
+#Power
+2**5
+
+
+# In[15]:
+
+
+#Root
+16**(1/2)
+
+
+# In[3]:
+
+
+num=12345
+last_digit=num%10
+last_digit
+
+
+# In[16]:
+
+
+num=54321
+last_digit=num%10
+last_digit
+
+
+# In[ ]:
+
+
+
+
